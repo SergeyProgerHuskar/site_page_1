3f074d269193a530cb3992d5b8520343c156d0b4
SergeyProgerHuskar <103854296+SergeyProgerHuskar@users.noreply.github.com>
2022-04-24 18:58:51 +0400

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,311 @@
+<html>
+
+<head>
+	<title>Марафон</title>
+	<link rel="shortcut icon" href="photo\89208.png" type="image/png">
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+	<div class="size">
+		<span class="letter_color_red">
+			<?php
+				echo '<script>document.write(document.body.clientHeight+"x"+document.body.clientWidth);</script>';
+			?>
+		</span>
+	</div>
+	<!-- <form>
+		<button class="button_registr">
+			<span class="letter_regular letter_color_white">
+				Зарегистрироваться
+			</span>
+		</button>
+	</form> -->
+
+	<!-- <form action="https://www.instagram.com/madina.wechat/" target="_blank">
+		<button class="button_inst_1">
+			<span class="letter_regular letter_opacity">
+				@madina.wechat
+			</span>
+		</button>
+	</form> -->
+
+<!-- ----------- -->
+<!-- <button type="button" class="btn btn-primary button_registr" data-bs-toggle="modal" data-bs-target="#exampleModal">
+						<span class="letter_regular letter_color_white">
+							Зарегистрироваться
+						</span>
+					</button>
+					<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+									<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+								</div>
+								<div class="modal-body">
+									<form>
+										<div class="mb-3">
+											<label for="exampleInputEmail1" class="form-label">Email address</label>
+											<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+											<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+										</div>
+										<div class="mb-3">
+											<label for="exampleInputPassword1" class="form-label">Password</label>
+											<input type="password" class="form-control" id="exampleInputPassword1">
+										</div>
+										<div class="mb-3 form-check">
+											<input type="checkbox" class="form-check-input" id="exampleCheck1">
+											<label class="form-check-label" for="exampleCheck1">Check me out</label>
+										</div>
+										<button type="submit" class="btn btn-primary">Submit</button>
+									</form>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+									<button type="button" class="btn btn-primary">Save changes</button>
+								</div>
+							</div>
+						</div>
+					</div> -->
+<!-- ----------- -->
+
+	<div class="backimage_1">
+		<div class="div_margin">
+			<div class="grid_container_2">
+				<div class="letter_date gc2_date_1"><span class="letter_color_white">Дата: 28-29 декабря</span></div>
+				<div class="letter_date gc2_date_2"><span class="letter_color_white">Время: 19:00</span></div>
+				<div class="letter_date gc2_date_3"><span class="letter_color_white">Формат: онлайн</span></div>
+				<div class="gc2_header letter_title_1">
+					<span class="letter_color_white">Бизнес с </span>
+					<span class="letter_color_red">Китаем</span>
+				</div>
+				<div class="gc2_text letter_regular letter_color_white">
+					Зарегистрируйся на двухдневный марафон и получи пошаговую систему,
+					как заработать первые 100 000 ₽ благодаря поставке из Китая
+				</div>
+				<div class="gc2_button">
+					123
+				</div>
+				<div class="gc2_photo">1</div>
+				<div class="gc2_content_1 comment_black_1">
+					<span class="letter_regular">
+						<span class="letter_color_red">16</span>
+						<span class="letter_color_white"> лет <br>в активных продажах</span>
+					</span>
+				</div>
+				<div class="gc2_content_2 comment_black_1">
+					<span class="letter_regular">
+						<span class="letter_color_white">более </span>
+						<span class="letter_color_red">1000</span><br>
+						<span class="letter_color_white"> выгодных сделок</span>
+					</span>
+				</div>
+				<div class="gc2_content_3 comment_black_1">
+					<span class="letter_regular">
+						<span class="letter_color_white">более </span>
+						<span class="letter_color_red">500</span><br>
+						<span class="letter_color_white"> успешных участников</span>
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- <div class="backimage_1">
+		<div class="div_margin">
+			<div class="grid_container">
+				<div class="item_11"><span class="letter_date_2 letter_color_white">Дата: 28-29 декабря</span></div>
+				<div class="item_12"><span class="letter_date_2 letter_color_white">Время: 19:00</span></div>
+				<div class="item_13"><span class="letter_date_2 letter_color_white">Формат: онлайн</span></div>
+				<div class="item_6 letter_title_1">
+					<span class="letter_color_white">Проект </span>
+					<span class="letter_color_red">Project</span>
+				</div>
+				<div class="item_photo"></div>
+				<div class="item_6 letter_regular letter_color_white">
+					!!! картинку фоном тут и сверху !!!<br>
+					Зарегистрируйся на двухдневный марафон и получи пошаговую систему, 
+					как заработать первые 100 000 ₽ благодаря поставке из Китая
+				</div>
+				<div class="item_14 comment_black_1">
+					<span class="letter_regular">
+						<span class="letter_color_red">16</span>
+						<span class="letter_color_white"> лет <br>в активных продажах</span>
+					</span>
+				</div>
+				<div class="item_15 comment_black_1">
+					<span class="letter_regular">
+						<span class="letter_color_white">более </span>
+						<span class="letter_color_red">1000</span><br>
+						<span class="letter_color_white"> выгодных сделок</span>
+					</span>
+				</div>
+				<div class="item_16 comment_black_1">
+					<span class="letter_regular">
+						<span class="letter_color_white">более </span>
+						<span class="letter_color_red">500</span><br>
+						<span class="letter_color_white"> успешных участников</span>
+					</span>
+				</div>
+			</div>
+		</div>
+	</div> -->
+
+	<br><br><br><br><br><br><br><br>
+	<br><br><br><br><br><br><br><br>
+
+	<div class="backimage_1">
+		<div class="div_margin">
+			<div class="letter_date">
+				<div><span class="letter_color_white">Дата: 28-29 декабря</span></div>
+				<div><span class="letter_color_white">Время: 19:00</span></div>
+				<div><span class="letter_color_white">Формат: онлайн</span></div>
+			</div>
+			<div class="letter_title_1">
+				<span class="letter_color_white">Проект </span>
+				<span class="letter_color_red">Project</span>
+			</div>
+			<div class="letter_regular letter_color_white">
+				Зарегистрируйся на двухдневный марафон и получи пошаговую систему,
+				как заработать первые 100 000 ₽ благодаря поставке из Китая
+			</div>
+			<br><br>
+			<div class="container_1">
+				<div class="comment_black_1">
+					<span class="letter_regular">
+						<span class="letter_color_red">16</span>
+						<span class="letter_color_white"> лет <br>в активных продажах</span>
+					</span>
+				</div>
+				<div class="comment_black_1">
+					<span class="letter_regular">
+						<span class="letter_color_white">более </span>
+						<span class="letter_color_red">1000</span><br>
+						<span class="letter_color_white"> выгодных сделок</span>
+					</span>
+				</div>
+				<div class="comment_black_1">
+					<span class="letter_regular">
+						<span class="letter_color_white">более </span>
+						<span class="letter_color_red">500</span><br>
+						<span class="letter_color_white"> успешных участников</span>
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="div_margin">
+		<div class="letter_title_2">
+			Что вас ждёт на марафоне
+		</div>
+		<div class="letter_regular">
+			<div class="comment_black_1">
+				<span class="letter_color_white">
+					Как работать с Китаем?<br>
+					Какой вид товара будет активно пользоваться спросом?<br>
+					Как найти поставщиков с Китая?<br>
+					Какие будут расходы, помимо покупки товара?<br>
+					Как заказывать с Китая товар?<br>
+					Где брать качественный товар и недорого?<br>
+					Как все это потом продавать?<br><br><br><br><br><br>
+				</span>
+			</div>
+			<div class="comment_1_white">
+				Как заработать первые 100 000 ₽ благодаря поставке из Китая<br><br>
+				5 мифов о работе с Китаем<br><br>
+				Кто сказал, что в Китае некачественные вещи (распаковка вещей в прямом эфире)?<br><br>
+				5 главных преимуществ работы с Китаем
+			</div>
+		</div>
+		<form>
+			<button class="button_1">
+				<span class="letter_regular letter_color_white">
+					Иду на марафон
+				</span>
+			</button>
+		</form>
+
+
+		<br><br><br><br><br><br><br><br>
+		<br><br><br><br><br><br><br><br>
+
+		<div class="letter_title_2">
+			Кому будет полезен марафон
+		</div>
+		<div class="letter_regular">
+			<div class="container_1">
+				<div class="comment_black_2">
+					<span class="letter_regular">
+						<span class="letter_color_red">01</span><br>
+						<span class="letter_color_white">Кто хочет открыть свой бизнес</span></span>
+				</div>
+				<div class="comment_black_2">
+					<span class="letter_regular">
+						<span class="letter_color_red">02</span><br>
+						<span class="letter_color_white">Кто хочет получить дополнительный доход без лишних вложений</span></span>
+				</div>
+			</div>
+			<div class="container_1">
+				<div class="comment_black_2">
+					<span class="letter_regular">
+						<span class="letter_color_red">03</span><br>
+						<span class="letter_color_white">Кто хочет покупать качественные вещи премиум класса для себя и своих близких</span></span>
+				</div>
+				<div class="comment_black_2">
+					<span class="letter_regular">
+						<span class="letter_color_red">04</span><br>
+						<span class="letter_color_white">Кто хочет работать с шоурумами и заниматься дропшиппингом</span></span>
+				</div>
+			</div>
+		</div>
+
+
+		<br><br><br><br><br><br><br><br>
+		<tt>Тут должна была быть картинка</tt>
+	</div>
+
+
+
+	<br><br><br><br><br><br><br><br>
+
+	<div class="backimage_1">
+		<div class="grid_container">
+			<div class="gc_1_item_a">
+				<div class="letter_title_2 letter_color_white">
+					Об авторе
+				</div>
+			</div>
+			<div class="gc_1_item_b">12</div>
+			<div class="gc_1_item_c">13</div>
+			<div class="gc_1_item_d">14</div>
+			<div class="gc_1_item_e">15</div>
+		</div>
+	</div>
+	<br><br><br><br><br><br><br><br>
+
+
+	<br><br><br><br><br><br><br><br>
+
+
+	<br><br><br><br><br><br><br><br>
+	<div class="block_this_inf">
+		<div class="this_inf">Мадина Алиева</div>
+		<div class="this_inf">@madina.wechat</div>
+		<div class="this_inf">2021 © Все права защищены</div>
+		<div class="this_inf">Политика конфиденциальности</div>
+	</div>
+	<div class="block_my_inf">
+		Made SSS
+	</div>
+
+
+
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
